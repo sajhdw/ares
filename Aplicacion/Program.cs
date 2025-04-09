8d5d95910a39f4038daf4a0b444eff0c4ce341ee
andre <nyxwallestein@gmail.com>
2025-04-09 10:25:59 -0500

    cambios de visual studio

--- Aplicacion/Program.cs
+++ Aplicacion/Program.cs
@@ -10,6 +10,7 @@ namespace Aplicacion
             Console.Write("Bienvenido al curso de c#");
             Console.Write("Bienvenido al curso de c#");
             Console.Write("Bienvenido al curso de c#");
+            Console.Write("Bienvenido al curso de c#");
             Console.ReadKey();
         }
     }
