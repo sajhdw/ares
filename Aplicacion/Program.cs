3ce138fe41c038fc87a560673338616645f12003
andre <nyxwallestein@gmail.com>
2025-04-09 10:03:47 -0500

    primer commit

---
+++ Aplicacion/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bienvenido al curso de c#");
+            Console.ReadKey();
+        }
+    }
+}
